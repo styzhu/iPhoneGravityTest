4b832abff0585abf152828ed81fdae4b5a198737
FunghiApe <zhu@junyuan.me>
2015-06-19 11:30:58 -0600

    add mobile camera tilt

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 dir = new Vector3(0f, 0f, 0f);
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			dir = new Vector3 (0f,  1f, 0f);
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			dir = new Vector3 (0f, -1f, 0f);
+		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			dir = new Vector3 ( 1f, 0f, 0f);
+		} else if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			dir = new Vector3 (-1f, 0f, 0f);
+		} else {
+			return;
+		}
+		transform.position += dir;
+	}
+}
---
+++ Assets/Scripts/StyCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StyCamera : MonoBehaviour
+{
+	public float smooth = 2.0f;
+	public float tiltAngle = 20.0f;
+
+	private float _rotDefaultX = 0f;
+	private float _rotDefaultY = 0f;
+	private float _rotDefaultZ = 0f;
+
+	private float _inputDefaultX = 0f;
+	private float _inputDefaultY = 0f;
+	private float _inputDefaultZ = 0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_rotDefaultX = transform.eulerAngles.x;
+		_rotDefaultY = transform.eulerAngles.y;
+		_rotDefaultZ = transform.eulerAngles.z;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.touchCount > 0) {
+			ResetTilt();
+		}
+
+		float tiltAroundY = -(Input.acceleration.x - _inputDefaultX) * tiltAngle;//Input.GetAxis("Horizontal") * tiltAngle;
+		float tiltAroundX = (Input.acceleration.y - _inputDefaultY) * tiltAngle;//Input.GetAxis("Vertical") * tiltAngle;
+		Debug.Log("x: "+tiltAroundX+" y: "+tiltAroundY);
+		Quaternion target = Quaternion.Euler(tiltAroundX + _rotDefaultX, tiltAroundY + _rotDefaultY, _rotDefaultZ);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+	}
+
+	void ResetTilt(){
+		_inputDefaultX = Input.acceleration.x;
+		_inputDefaultY = Input.acceleration.y;
+		_inputDefaultZ = Input.acceleration.z;
+	}
+}
